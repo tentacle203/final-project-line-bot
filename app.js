477ba400df8295db2d381d343ebd0782df2b6719
tentacle203 <jasonliao0109@gmail.com>
2024-12-05 21:23:06 +0800

    test new example codes

--- app.js
+++ app.js
@@ -1,49 +1,17 @@
-require('dotenv').config();
+var linebot = require('linebot');
 
-const line = require('@line/bot-sdk');
-const express = require('express');
+var bot = linebot({
+    channelId: CHANNEL_ID,
+    channelSecret: CHANNEL_SECRET,
+    channelAccessToken: CHANNEL_ACESS_TOKEN
+});
 
-// create LINE SDK config from env variables
-const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
-};
-
-// create LINE SDK client
-const client = new line.Client(config);
-
-// create Express app
-// about Express itself: https://expressjs.com/
-const app = express();
-
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
+bot.on('message', function (event) {
+    event.reply(event.message.text).then(function (data) {
+      // 當訊息成功回傳後的處理
+    }).catch(function (error) {
+      // 當訊息回傳失敗後的處理
     });
-});
+  });
 
-// event handler
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
-
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
-}
-
-// listen on port
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
+bot.listen('/linewebhook', 100);
